82cf1b7a27023bcc193aaa335e13fb3ead326f65
Lucas Cava <lucas.cava@hotmail.com>
2024-05-08 03:42:23 -0300

    refactor: add config to vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+resource "aws_security_group" "lambda_auth_sg" {
+  name   = "lambda_auth_sg"
+  vpc_id = data.aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lambda_function" "lambda_auth" {
   filename      = "lambda/lambda_function.zip"
   function_name = var.lambda_function_name
@@ -13,11 +25,21 @@ resource "aws_lambda_function" "lambda_auth" {
     }
   }
 
-  # vpc_config {
-  #   subnet_ids         = [data.aws_subnet.public_subnet.id, data.aws_subnet.private_subnet.id]
-  #   security_group_ids = [data.aws_security_group.rds_public_sg.id]
-  # }
+  vpc_config {
+    subnet_ids         = [data.aws_subnet.public_subnet.id, data.aws_subnet.private_subnet.id]
+    security_group_ids = [aws_security_group.lambda_auth_sg.id]
+  }
 }
+
+resource "aws_security_group_rule" "lambda_to_rds" {
+  type                     = "ingress"
+  from_port                = 5432 # Porta do banco de dados na instância RDS
+  to_port                  = 5432 # Porta do banco de dados na instância RDS
+  protocol                 = "tcp"
+  security_group_id        = data.aws_security_group.rds_public_sg.id # ID da security group da RDS
+  source_security_group_id = aws_security_group.lambda_auth_sg.id     # ID da security group da Lambda
+}
+
 
 /**
  * AWS IAM Role for Lambda
@@ -52,7 +74,7 @@ resource "aws_iam_role" "lambda_role" {
 resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   name       = var.lambda_policy_attachment_name
   roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
 }
 
 /**
@@ -64,8 +86,8 @@ resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
   */
 resource "aws_iam_policy_attachment" "lambda_rds_policy" {
   name       = "lambda_rds_policy"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
   roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
 }
 
 /**
@@ -84,6 +106,9 @@ resource "aws_iam_policy" "ssm_access_policy" {
       {
         Effect = "Allow",
         Action = [
+          # "ssm:Describe*",
+          #       "ssm:Get*",
+          #       "ssm:List*"
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
@@ -111,35 +136,16 @@ resource "aws_iam_role_policy_attachment" "lambda_ssm_access" {
 }
 
 /**
-  * AWS IAM Policy for VPC Access
-  *
-  * This resource block defines an AWS IAM policy that allows Lambda functions to access
-  * VPC resources.
-  */
-resource "aws_iam_policy" "lambda_vpc_policy" {
-  name        = "LambdaVpcPolicy"
-  description = "Policy to allow Lambda to access VPC resources"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = "ec2:CreateNetworkInterface",
-      Resource = "*",
-    }],
-  })
-}
-
-/**
   * Attach the AWS IAM policy to the Lambda function's role.
   *
   * This resource block attaches the AWS IAM policy specified by the `policy_arn` attribute
   * to the IAM role associated with the Lambda function. The `name` attribute is used to
   * provide a unique name for this policy attachment resource.
   */
-resource "aws_iam_role_policy_attachment" "lambda_vpc_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_vpc_policy.arn
+resource "aws_iam_policy_attachment" "lambda_vpc_policy" {
+  name       = "lambda_vpc_policy"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
 }
 
 terraform {
