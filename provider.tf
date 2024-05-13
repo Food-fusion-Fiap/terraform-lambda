db2f9ce207ca0b6f79a896aa311f82cb30b0f0c0
pspalmirasaraaf <pspalmirasaraaf@gmail.com>
2024-05-13 18:19:28 -0300

    change provider name

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-fiap-group-18"
+    bucket = "terraform-github-action"
     key    = "prod/terraform-lambda.tfstate"
     region = "us-east-1"
   }
